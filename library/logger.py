2d79949be1c755a23939cee5bd9649f446270502
Preethi Prabhakaran <preethi.prabhakaran.testing@gmail.com>
2025-01-24 13:02:41 +0530

    Cleaned up the repo by removing old files

--- library/logger.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-
-def log_event():
-    logging.basicConfig(filename="test" + __name__ + ".log", level=logging.INFO,
-                        format="%(asctime)s - %(levelname)s - %(message)s")
-    return logging
