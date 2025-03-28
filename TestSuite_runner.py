d2be68edeeae2be987d27f2b23cee9e487b17f2f
Preethi Prabhakaran <preethi.prabhakaran.testing@gmail.com>
2025-03-28 13:28:06 +0530

    Modified runner file to include test results in an xml file

--- TestSuite_runner.py
+++ TestSuite_runner.py
@@ -1,4 +1,7 @@
 import unittest
+
+import xmlrunner
+
 from tests import Buzz, PIM, Recruitment
 
 
@@ -12,10 +15,10 @@ if __name__ == '__main__':
         all_tests = unittest.TestSuite([suite_pim, suite_recruitment, suite_buzz])
         return all_tests
 
-    # initialize a runner, pass it to your suite and run it
+    # initialize a runner XMLRunner to get output as xml, pass it to your suite and run it
     # output the test results to an xml file
-    with open('test_run_result.xml', 'w') as results:
-        runner = unittest.TextTestRunner(results, verbosity=3)
+    with open('test_results/test_run_result.xml', 'w') as results:
+        runner = xmlrunner.XMLTestRunner(results, verbosity=3)
         runner.run(suites())
 
 
